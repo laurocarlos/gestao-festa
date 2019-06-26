2550e34b7e5ae4a82b8ed22f8ed994a1c29c3ee5
aarao_rj@hotmail.com <Lacp1007@@>
2019-06-26 11:24:14 -0300

    Initial Commit

---
+++ src/main/java/br/com/lapaz/festa/controller/ConvidadosController.java
@@ -0,0 +1,12 @@
+package br.com.lapaz.festa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConvidadosController {
+	@GetMapping("/convidados")
+	public String list() {
+		return "Lista de Convidados";
+	}
+}
